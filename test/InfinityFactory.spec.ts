b4da026f2dce9ffb3a5b93c8dd77773e16dfe62a
Arons <>
2021-02-21 20:07:35 +0000

    Test update

--- test/InfinityFactory.spec.ts
+++ test/InfinityFactory.spec.ts
@@ -68,7 +68,7 @@ describe('InfinityFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2512920)
+    expect(receipt.gasUsed).to.eq(3344652)
   })
 
   it('setFeeTo', async () => {
